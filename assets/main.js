0f6a4bd2dd0a8a98a14fbd42aa779710ddbe368e
Valentina De Mite <valesakura97@gmail.com>
2023-09-07 14:26:20 +0200

    fisrst commit

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+/*  Carosello Array di Oggetti
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto di un array di stringhe.
+
+Bonus 0:
+Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le funzioni. É possibile fare refactoring del codice, pulendolo e creando quanche funzione che possa rendere tutto piú leggibile e pulito?
+
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello?
+*/
